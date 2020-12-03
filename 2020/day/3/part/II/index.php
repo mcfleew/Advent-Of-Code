bead96205bdca801b890b96c4bd2dd5ea3a9d89a
mcfleew <you@example.comfleury.timbe@gmail.com>
2020-12-03 13:12:28 +0100

    Pretty code S/O @MaximeDezette

--- 2020/day/3/part/II/index.php
+++ 2020/day/3/part/II/index.php
@@ -2,34 +2,36 @@
 
 $input = array_map('trim', file('../../input'));
 
-function testSlope($step = 1, $bottomStep = 1) {
+function checkSlope($rightStep = 1, $downStep = 1) {
     global $input, $debug, $output;
 
-    $index = $countTrees = 0;
-    $repeat = 2;
+    $colIndex = $countTrees = 0;
+    $multiplier_repeat = 2;
 
-    foreach ($input as $key => $row) {
+    foreach ($input as $rowIndex => $mapString) {
         // Skip rows if needed DownStep
-        if ($rowToSkip = ($key % $bottomStep > 0)) {
+        if ($rowToSkip = ($rowIndex % $downStep > 0)) {
             continue;
         }
-        $debug[intval($bottomStep).$step][$key] = $index;
-        $output[$key][$index] = TOKEN_IF_OK;
+        // $debug[intval($downStep).$rightStep][$rowIndex] = $colIndex;
         
         // Repeat map's pattern if index greater than map length
-        if ($index >= strlen($row)) {
-            $row = str_repeat($row, $repeat); // $index -= count($map) - 1;
-            $repeat++;
+        if ($colIndex >= strlen($mapString)) {
+            $mapString = str_repeat($mapString, $multiplier_repeat); // $colIndex -= count($map) - 1;
+            $multiplier_repeat++;
         }
     
-        $map = str_split($row);
+        // Turn row string into array
+        $mapArray = str_split($mapString);
     
         // Looking for trees depend of RightStep and DownStep defined by actual slope
-        if ($map[$index] === '#') {
-            $output[$key][$index] = TOKEN_IF_KO;
+        if ($mapArray[$colIndex] === '#') {
+            $output[$rowIndex][$colIndex] = TOKEN_IF_KO;
             $countTrees++;
+        } else {
+            $output[$rowIndex][$colIndex] = TOKEN_IF_OK;
         }
-        $index += $step;
+        $colIndex += $rightStep;
     }
 
     return $countTrees;
@@ -37,11 +39,11 @@ function testSlope($step = 1, $bottomStep = 1) {
 
 $debug = [];
 
-// echo testSlope().PHP_EOL;
-// echo testSlope(3).PHP_EOL;
-// echo testSlope(5).PHP_EOL;
-// echo testSlope(7).PHP_EOL;
-// echo testSlope(1, 2).PHP_EOL;
+// echo checkSlope().PHP_EOL;
+// echo checkSlope(3).PHP_EOL;
+// echo checkSlope(5).PHP_EOL;
+// echo checkSlope(7).PHP_EOL;
+// echo checkSlope(1, 2).PHP_EOL;
 
 // print_r($debug);
 
@@ -53,12 +55,12 @@ define('TOKEN_IF_KO', 'X');
 
 $output = array_fill_keys(range(0, ROWS_TO_SHOW), array_fill_keys(range(0, COLS_TO_SHOW), TOKEN_IF_MEH));
 
-echo (testSlope() * testSlope(3) * testSlope(5) * testSlope(7) * testSlope(1, 2)).str_repeat(PHP_EOL, 2);
+echo (checkSlope() * checkSlope(3) * checkSlope(5) * checkSlope(7) * checkSlope(1, 2)).str_repeat(PHP_EOL, 2);
 
-// Show map
-$i = 0;
-array_map(function ($map) {
-    global $i;
-    echo $i.' : '.implode('', $map).PHP_EOL;
-    $i++;
+// Show full map
+$rowId = 0;
+array_map(function ($fullMap) {
+    global $rowId;
+    echo $rowId.' : '.implode('', $fullMap).PHP_EOL;
+    $rowId++;
 }, $output);
